a22906b48a448e9cdece7aff6d66ae678d70abe0
vladstoian <vlad.stoian98@gmail.com>
2024-01-11 20:24:04 +0200

    Proiect aproape final

---
+++ Pages/AuthorBooks.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OnlineLibrary.Data;
+using OnlineLibrary.Models;
+
+namespace OnlineLibrary.Pages
+{
+    public class AuthorBooksModel : PageModel
+    {
+        private readonly LibraryDbContext libraryDbContext;
+
+        public AuthorBooksModel(LibraryDbContext libraryDbContext)
+        {
+            this.libraryDbContext = libraryDbContext;
+        }
+
+        public IList<Book> Books { get; set; }
+        public string AuthorName { get; set; }
+
+        public void OnGet(string authorName)
+        {
+            AuthorName = authorName;
+            Books = libraryDbContext.Books
+                .Where(b => b.Author == authorName)
+                .ToList();
+        }
+
+        public string GetPdfDownloadUrl(string pdfPath)
+        {
+            var fullPath = Url.Content(Path.Combine("~/", pdfPath));
+            return fullPath;
+        }
+    }
+}
---
+++ Pages/GetAuthors.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineLibrary.Data;
+
+namespace OnlineLibrary.Pages
+{
+    public class GetAuthorsModel : PageModel
+    {
+        private readonly LibraryDbContext libraryDbContext;
+
+        public GetAuthorsModel(LibraryDbContext libraryDbContext)
+        {
+            this.libraryDbContext = libraryDbContext;
+        }
+
+        public IList<string> Authors { get; set; }
+
+        public void OnGet()
+        {
+            //Authors = libraryDbContext.Users
+            //    .Where(u => u.Authority == "Author");
+
+            Authors = libraryDbContext.Books
+                .Select(b => b.Author)
+                .Distinct()
+                .ToList();
+        }
+    }
+}
